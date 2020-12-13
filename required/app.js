9400502085c1fc417ce3e3c23e50ecfcc6703df6
walaha2002 <68665730+walaha2002@users.noreply.github.com>
2020-12-13 00:43:11 -0600

    - Adds plot id tag to html file

--- required/app.js
+++ required/app.js
@@ -10,5 +10,18 @@ d3.json("/data/samples.json").then((data) => {
         y: data.otu_labels,
         type: 'horizontalBar',
         name: "Operational Taxonomic Units"
-    }
+    };
+
+    //Create the data array for the plot
+    var data = [trace1];
+
+    //Define the plot layout
+    var layout = {
+        title: "Operational Taxonomic Units"
+        xaxis: {title: "something"},
+        yaxis: {title: "something else"}
+    };
+
+    //Plot the chart to a div tag with id "plot"
+    Plotly.newPlot("plot",data,layout);
 });
