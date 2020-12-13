144bfab8cb199944fe8e386fde0e15fd7227eae7
walaha2002 <68665730+walaha2002@users.noreply.github.com>
2020-12-13 01:28:11 -0600

    - Attempts to sort but encounters error

--- required/app.js
+++ required/app.js
@@ -3,26 +3,46 @@
 console.log(d3.json("/data/samples.json"));
 
 d3.json("/data/samples.json").then((samples) => {
+    const data = samples;
 
-    //Create the Traces
-    var trace1 = {
-        x: samples.sample_values,
-       // y: samples.otu_labels,
-        type: 'horizontalBar',
-        name: "Operational Taxonomic Units",
-        label: samples.otu_labels
-    };
 
-    //Create the data array for the plot
-    var data = [trace1];
+    // Sort descending to then get the top 10 OTUs
+    // data.sort((a,b) => a-b);
+    // console.log(data);
+    
+    data.sort(function (a, b) {
+        return (b.sample_values) - (a.sample_values);
+    });
 
-    //Define the plot layout
-    var layout = {
-        title: "Operational Taxonomic Units",
-        xaxis: {title: "something"},
-        yaxis: {title: "something else"}
-    };
+    // Slice the first 10 objects for plotting
+    data = data.slice(0, 10);
 
-    //Plot the chart to a div tag with id "plot"
-    Plotly.newPlot("plot",data,layout);
+    // Reverse the array
+    data = data.reverse();
+
+    console.log(data);
+    
 });
+
+//     //Create the Traces
+//     var trace1 = {
+//         x: samples.names,
+//         y: samples.otu_labels,
+//         type: 'horizontalBar',
+//         name: "Operational Taxonomic Units",
+//         label: samples.otu_labels
+//     };
+
+//     //Create the data array for the plot
+//     var data = [trace1];
+
+//     //Define the plot layout
+//     var layout = {
+//         title: "Operational Taxonomic Units",
+//         xaxis: {title: "something"},
+//         yaxis: {title: "something else"}
+//     };
+
+//     //Plot the chart to a div tag with id "plot"
+//     Plotly.newPlot("plot",data,layout);
+// });
