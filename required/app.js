fe4022d3ee353b68f02bba9855cb95e774bc9470
walaha2002 <68665730+walaha2002@users.noreply.github.com>
2020-12-13 00:39:11 -0600

    - Attempts to begin trace

--- required/app.js
+++ required/app.js
@@ -1,3 +1,14 @@
 // Read in json data
 // const data = d3.json("data/samples.json");
 console.log(d3.json("/data/samples.json"));
+
+d3.json("/data/samples.json").then((data) => {
+
+    //Create the Traces
+    var trace1 = {
+        x: data.sample_values,
+        y: data.otu_labels,
+        type: 'horizontalBar',
+        name: "Operational Taxonomic Units"
+    }
+});
